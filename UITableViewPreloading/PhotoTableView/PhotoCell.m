514937eab540e219c3ed83cbc0fa40f2744c2267
YANGGUANG <yangguang@eku.com.cn>
2018-08-04 20:14:02 +0800

    add exception domain.

--- UITableViewPreloading/PhotoTableView/PhotoCell.m
+++ UITableViewPreloading/PhotoTableView/PhotoCell.m
@@ -76,7 +76,7 @@
 
     __weak PhotoCell *wself = self;
     self.dateLabel.text = self.data.dateString;
-    [_pictureView sd_setImageWithURL:[NSURL URLWithString:self.data.url] placeholderImage:nil options:SDWebImageForceTransition completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+    [_pictureView sd_setImageWithURL:[NSURL URLWithString:self.data.url] placeholderImage:nil options:SDWebImageAllowInvalidSSLCertificates completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
         #warning 加载的时候闪一下是因为下边的动画, 注释掉就不闪了
         wself.pictureView.image = image;
 //        wself.pictureView.alpha = 0;
