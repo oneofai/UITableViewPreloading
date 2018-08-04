c472753d4246fe1a3fa94c85607200a1547d2bc9
YANGGUANG <yangguang@eku.com.cn>
2018-08-04 10:21:24 +0800

    Initial Commit

---
+++ UITableViewPreloading/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableViewPreloading
+//
+//  Created by Sun on 2018/8/4.
+//  Copyright © 2018年 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
